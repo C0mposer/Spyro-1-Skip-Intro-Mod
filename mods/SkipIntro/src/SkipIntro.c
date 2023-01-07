052c530be8cd1dd4ae137bda61ee6fbc3ed02d2e
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 05:55:43 -0800

    Added comments

--- mods/SkipIntro/src/SkipIntro.c
+++ mods/SkipIntro/src/SkipIntro.c
@@ -1,14 +1,14 @@
 #include <custom_types.h>
 #include <common.h>
 
-bool hasRan = FALSE;
+bool hasRan = FALSE;                               //? Flag to only run this code once
 
 void mainFunc()
 {
-   if(!hasRan && _globalTimer > 20)
+   if(!hasRan && _globalTimer > 20)                //? If the code hasn't ran once yet, and the global timer is greater than 20. Checking global timer because I have to wait a few frames to call The Adventure Begins
    {
-      _isPastTitleScreen = TRUE;
-      TheAdventureBegins();
-      hasRan = TRUE;
+      _isPastTitleScreen = TRUE;                   //? This flag is checked by TheAdventureBeings() before it runs, so we must set this flag to TRUE.
+      TheAdventureBegins();                        //? Call The Adventure Begins cutscene sequence
+      hasRan = TRUE;                               //? Set hasRan to TRUE causing this code to never run again
    }
 }
