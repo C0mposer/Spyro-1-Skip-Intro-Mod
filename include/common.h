792cabbb3df6073b1083c8b970946a984176fe23
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 05:24:54 -0800

    Added Git Repo

---
+++ include/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "custom_types.h"
+
+void TheAdventureBegins();                                   //? This function calls the "The Adventure Begins" start sequence in the game.
+
+
+extern bool _isPastTitleScreen; //0x8007566c                 //? TRUE If has started new game/continue game. Basically after you leave the title screen this is always TRUE.
+
+extern int _globalTimer; //0x800749e0                        //? This is the global timer. It is running at all times.                
+
+#endif /* COMMON_H */
