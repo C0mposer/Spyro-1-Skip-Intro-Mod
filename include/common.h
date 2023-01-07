eb74801d33b5a1ed7def7d5f84f602d2e41ff1c9
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 09:03:43 -0800

    Renamed Hook

--- include/common.h
+++ include/common.h
@@ -8,7 +8,6 @@ extern void TheAdventureBegins();                            //? This function c
 
 //* ~ Symbols ~
 extern bool _isPastTitleScreen; //0x8007566c                 //? TRUE If has started new game/continue game. Basically after you leave the title screen this is always TRUE.
-
 extern int _globalTimer; //0x800749e0                        //? This is the global timer. It is running at all times from game boot up past BIOS.                
 
 #endif /* COMMON_H */
